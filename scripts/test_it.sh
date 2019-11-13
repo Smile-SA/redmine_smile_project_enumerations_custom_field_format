c3c0b34e3e2fd95b7e358f2a08a1eaf1fa9e2076
Lxc <mel+lxc@ennder.fr>
2019-11-13 21:59:04 +0100

    example to launch a single test
    modifié :         scripts/test_it.sh

--- scripts/test_it.sh
+++ scripts/test_it.sh
@@ -2,5 +2,7 @@
 
 cd ../..
 
+#RAILS_ENV=test rake test TEST=plugins/redmine_smile_project_enumerations_custom_field_format/test/functional/issues_controller_test.rb TESTOPTS="-n /test_post_create/"
+
 RAILS_ENV=test rails redmine:plugins:test NAME=redmine_smile_project_enumerations_custom_field_format
 
