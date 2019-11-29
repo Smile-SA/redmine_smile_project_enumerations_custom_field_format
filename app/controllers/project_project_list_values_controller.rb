338c2439c8d7e9afc2a30916f246062876bc45b7
Lxc <mel+lxc@ennder.fr>
2019-11-29 16:44:56 +0100

    new files for list values custom field format
    nouveau fichier : app/controllers/project_project_list_values_controller.rb
    nouveau fichier : app/views/custom_fields/formats/_project_list_value.html.erb
    nouveau fichier : app/views/project_project_list_values/_form.html.erb
    nouveau fichier : app/views/project_project_list_values/edit.html.erb
    nouveau fichier : app/views/project_project_list_values/new.html.erb
    nouveau fichier : app/views/projects/settings/_project_list_values.html.erb
    nouveau fichier : lib/project_list_value_field_format.rb

---
+++ app/controllers/project_project_list_values_controller.rb
@@ -0,0 +1,142 @@
+# Redmine - project management software
+# Copyright (C) 2006-2017  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class ProjectProjectListValuesController < ApplicationController
+  menu_item :issues
+  model_object ProjectEnumeration
+
+  before_action :find_model_object, :except => [:new, :create]
+  before_action :find_project_from_association, :except => [:new, :create]
+  before_action :find_project_by_project_id, :only => [:new, :create]
+  before_action :find_custom_field_by_custom_field_id, :only => [:new, :create]
+
+  before_action :authorize
+
+
+  # TODO create API views
+  accept_api_auth :create, :update, :destroy
+
+  helper :projects
+
+
+  def new
+    @project_list_value = ProjectEnumeration.new(
+        :project_id => @project.id,
+        :custom_field_id => @custom_field.id
+      )
+
+    @project_list_value.safe_attributes = params[:project_enumeration]
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def create
+    @project_list_value = ProjectEnumeration.new(
+        :project_id => @project.id,
+      )
+
+    if params[:project_enumeration]
+      attributes = params[:project_enumeration].dup
+      attributes.delete('sharing') unless attributes.nil? || @project_list_value.allowed_sharings.include?(attributes['sharing'])
+
+      @project_list_value.custom_field_id = @custom_field.id
+
+      @project_list_value.safe_attributes = attributes
+    end
+
+    if request.post?
+      if @project_list_value.save
+        respond_to do |format|
+          format.html do
+            flash[:notice] = l(:notice_successful_create)
+            redirect_back_or_default settings_project_path(@project, :tab => 'project_list_values')
+          end
+          format.js
+          format.api do
+            render :action => 'show', :status => :created, :location => project_list_value_url(@project_list_value)
+          end
+        end
+      else
+        respond_to do |format|
+          format.html { render :action => 'new' }
+          format.js   { render :action => 'new' }
+          format.api  { render_validation_errors(@project_list_value) }
+        end
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:project_enumeration]
+      attributes = params[:project_enumeration].dup
+      attributes.delete('sharing') unless @project_list_value.allowed_sharings.include?(attributes['sharing'])
+
+      #@project_list_value.custom_field_id = params[:custom_field_id]
+
+      @project_list_value.safe_attributes = attributes
+      if @project_list_value.save
+        respond_to do |format|
+          format.html {
+            flash[:notice] = l(:notice_successful_update)
+            redirect_back_or_default settings_project_path(@project, :tab => 'project_list_values')
+          }
+          format.api  { render_api_ok }
+        end
+      else
+        respond_to do |format|
+          format.html { render :action => 'edit' }
+          format.api  { render_validation_errors(@project_list_value) }
+        end
+      end
+    end
+  end
+
+  def destroy
+    @project_list_value.destroy
+    respond_to do |format|
+      format.html {
+        flash[:notice] = l(:notice_successful_delete)
+        redirect_back_or_default settings_project_path(@project, :tab => 'project_list_values')
+      }
+      format.api  { render_api_ok }
+    end
+  end
+
+
+  protected
+
+  def find_model_object
+    model = self.class.model_object
+    if model
+      @object = @project_list_value = model.find(params[:id])
+    end
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+
+  def find_custom_field_by_custom_field_id
+    @custom_field = CustomField.find(params[:custom_field_id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+end
---
+++ lib/project_list_value_field_format.rb
@@ -0,0 +1,71 @@
+# Smile - redmine_smile_project_list_values_custom_field_format enhancement
+#
+# Compatible with Redmine 4.0
+#
+# module Redmine::FieldFormat::ProjectEnumerationFormat
+#
+# * InstanceMethods
+#   * possible_values_options
+#   * possible_values_list_values
+#   * protected
+#     * query_filter_values
+#     * possible_values_list_values
+#     * filtered_list_values_options
+
+
+module Redmine
+  module FieldFormat
+    class ProjectListValueFormat < RecordList
+      add 'project_list_value'
+      self.form_partial = 'custom_fields/formats/project_list_value'
+      field_attributes :version_status
+
+      def possible_values_options(custom_field, object=nil)
+        possible_values_list_values(custom_field, object).sort.collect{|v| [v.to_s, v.id.to_s] }
+      end
+
+      def before_custom_field_save(custom_field)
+        super
+        if custom_field.version_status.is_a?(Array)
+          custom_field.version_status.map!(&:to_s).reject!(&:blank?)
+        end
+      end
+
+      def target_class
+        @target_class ||= ProjectEnumeration
+      end
+
+      protected
+
+      def query_filter_values(custom_field, query)
+        versions = possible_values_list_values(custom_field, query.project, true)
+        ProjectEnumeration.sort_by_status(versions).collect{|s| ["#{s.project.name} - #{s.name}", s.id.to_s, l("version_status_#{s.status}")] }
+      end
+
+      def possible_values_list_values(custom_field, object=nil, all_statuses=false)
+        if object.is_a?(Array)
+          projects = object.map {|o| o.respond_to?(:project) ? o.project : nil}.compact.uniq
+          projects.map {|project| possible_values_list_values(custom_field, project)}.reduce(:&) || []
+        elsif object.respond_to?(:project) && object.project
+          scope = object.project.shared_list_values.joins(:custom_field).where('custom_fields.id = ?', custom_field.id)
+          filtered_list_values_options(custom_field, scope, all_statuses)
+        elsif object.nil?
+          scope = ::ProjectEnumeration.visible.where(:sharing => 'system')
+          filtered_list_values_options(custom_field, scope, all_statuses)
+        else
+          []
+        end
+      end
+
+      def filtered_list_values_options(custom_field, scope, all_statuses=false)
+        if !all_statuses && custom_field.version_status.is_a?(Array)
+          statuses = custom_field.version_status.map(&:to_s).reject(&:blank?)
+          if statuses.any?
+            scope = scope.where(:status => statuses.map(&:to_s))
+          end
+        end
+        scope
+      end
+    end
+  end # FieldFormatOverride
+end # module RedmineOverride
