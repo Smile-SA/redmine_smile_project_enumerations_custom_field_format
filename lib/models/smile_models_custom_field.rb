036dc63ed517601e9e2181cc9311e19846c9c2e7
Lxc <mel+lxc@ennder.fr>
2019-12-07 18:44:40 +0100

    for_project -> enabled_on_project, +not_enabled_on_project, +joins_projects
    modifié :         lib/models/smile_models_custom_field.rb

--- lib/models/smile_models_custom_field.rb
+++ lib/models/smile_models_custom_field.rb
@@ -9,9 +9,24 @@ module Smile
 
         def self.prepended(base)
           base.class_eval do
-            scope :for_project, lambda { |project|
-              joins("LEFT JOIN #{table_name_prefix}custom_fields_projects#{table_name_suffix} AS cfp ON cfp.custom_field_id = #{CustomField.table_name}.id").
-              where('cfp.project_id' => project.id)
+            scope :joins_projects, lambda {
+              joins("LEFT JOIN #{table_name_prefix}custom_fields_projects#{table_name_suffix} AS cfp ON cfp.custom_field_id = #{CustomField.table_name}.id")
+            }
+
+            scope :enabled_on_project, lambda { |project|
+              joins_projects.
+              where('cfp.project_id' => project.id).
+              or(
+                joins_projects.
+                where.not(:type => 'IssueCustomField')
+              ).
+              distinct
+            }
+
+            scope :not_enabled_on_project, lambda { |project|
+              enabled_project_ids = Project.joins_custom_fields.where(:id => project.id).pluck('cfp.custom_field_id')
+              where.not('id' => enabled_project_ids).
+              where(:type => 'IssueCustomField')
             }
           end
         end
