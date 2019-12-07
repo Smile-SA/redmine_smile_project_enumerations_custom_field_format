6baad137805d494b2c71db955ce0ba447f89ba18
Lxc <mel+lxc@ennder.fr>
2019-12-07 18:39:42 +0100

    routes to use simgle page to edit Project List Values
    modifié :         config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,23 @@ resources :projects do
     end
 
     member do
-      post 'create', :as => 'create'
-      put 'update', :as => 'update'
+      post 'create',     :as => 'create'
+      put 'update',      :as => 'update'
       put 'update_each', :as => 'update_each'
-      delete 'destroy', :as => 'destroy'
+      delete 'destroy',  :as => 'destroy'
     end
   end
 
-  resource :project_list_values, :controller => 'project_project_list_values', :only => [:new, :create, :edit, :update, :destroy]
+  resource :project_list_values, :controller => 'project_project_list_values', :only => [:new, :edit] do
+    collection do
+      get 'index'
+    end
+
+    member do
+      post 'create',     :as => 'create'
+      put 'update',      :as => 'update'
+      put 'update_each', :as => 'update_each'
+      delete 'destroy',  :as => 'destroy'
+    end
+  end
 end
